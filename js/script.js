9974ecdb34f56e701117825b33a354c4d6c21e05
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-30 16:15:04 +0200

    Add changing background color when changin the mouse position

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const h1 = document.querySelector('h1');
 document.body.addEventListener('mousemove', function(e) {
-  //h1.textContent = `${e.clientX}, ${e.clientY}`
-  h1.textContent = `${e.pageX}, ${e.pageY}`
-  h1.textContent = `${e.screenX}, ${e.screenY}`
+  //h1.textContent = `${e.clientX}, ${e.clientY}`;
+  //h1.textContent = `${e.pageX}, ${e.pageY}`;
+  h1.textContent = `${e.screenX}, ${e.screenY}`;
+
+  document.body.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY}, ${e.clientX/e.clientY * 20})`;
 })
